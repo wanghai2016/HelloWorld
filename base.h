e2449d1b0cd3bb2e191ab239f4d38e109140b20e
wanghai <wanghai@qiyi.com>
2017-08-24 10:42:18 +0800

    aaaaaa

--- base.h
+++ base.h
@@ -8,11 +8,7 @@ int show(int a);
 
 int add(int a);
 
-<<<<<<< HEAD
 int sub(int a);
 
 int div(int a);
 
-
-=======
->>>>>>> parent of 6f932cf... func sub
--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+
+
 #include "base.h"
 #include <iostream>
 using namespace std;
